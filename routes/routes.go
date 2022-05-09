9bfee5ce45dfeb685c5546e27836c1e4c49013ca
Fadel <dheroefic@gmail.com>
2022-05-09 12:59:14 +0700

    [UPDATE] Minor route update

--- routes/routes.go
+++ routes/routes.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/dheroefic/gin-boilerplate/database"
 	"github.com/dheroefic/gin-boilerplate/models/structs"
 	"github.com/dheroefic/gin-boilerplate/utils/helpers"
 	"github.com/gin-gonic/gin"
 )
 
+// All route defined here
 func Load(route *gin.Engine) {
+
 	// 404 states
 	route.NoRoute(func(ctx *gin.Context) {
 		helpers.Logger("ROUTING PROCESS", fmt.Sprintf("accessing undefined endpoint: %s", ctx.Request.RequestURI), false)
@@ -20,7 +23,13 @@ func Load(route *gin.Engine) {
 	// Health
 	// Default health-check route
 	route.GET("/health", func(ctx *gin.Context) {
-		obj := structs.APIResponse{Code: 1, Message: "System is normal"}
+		db := database.GetSession().Begin()
+		obj := structs.APIResponse{Code: http.StatusOK, Message: "System is normal"}
+		if err := db.Exec("select 1 + 1").Error; err != nil {
+			obj.Code = http.StatusInternalServerError
+			obj.Message = "There's something wrong with the system"
+		}
+		db.Commit()
 		ctx.SecureJSON(http.StatusOK, obj)
 	})
 
