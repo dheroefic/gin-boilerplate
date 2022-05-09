8f83cf6f1408495fa5c143bc3960c66c8560214f
Ahmad Fadhel <dheroefic@gmail.com>
2022-05-09 12:41:15 +0700

    [UPDATE] Minor database adjustment

--- database/database.go
+++ database/database.go
@@ -7,45 +7,46 @@ import (
 	"time"
 
 	"github.com/dheroefic/gin-boilerplate/utils/helpers"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-type (
-	DBInterface interface {
-		OpenConnection() (*gorm.DB, error)
-	}
-)
-
 var sqlConnection *gorm.DB
 
-func OpenConnection() (*gorm.DB, error) {
+func InitConnection() *gorm.DB {
+	var err error
 	host := os.Getenv("DATABASE_HOST")
 	port := os.Getenv("DATABASE_PORT")
 	user := os.Getenv("DATABASE_USER")
 	password := os.Getenv("DATABASE_PASSWORD")
 	name := os.Getenv("DATABASE_NAME")
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, name)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	helpers.Logger("DATABASE", "Initializing database connection...", false)
+
+	sqlConnection, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true,
+	})
+
 	if err != nil {
 		helpers.Logger("DATABASE", "Cannot open connection to database", true)
 	}
 
-	return db, err
-}
-
-func InitConnection() *gorm.DB {
-	sqlConnection, _ = OpenConnection()
 	sqlDB, _ := sqlConnection.DB()
-	helpers.Logger("DATABASE", "Initializing database connection...", false)
 	maxConn, _ := strconv.Atoi(os.Getenv("DATABASE_MAX_CONNECTION"))
+	maxIdleConn, _ := strconv.Atoi(os.Getenv("DATABASE_MAX_IDLE_CONNECTION"))
 	maxLifeTime, _ := strconv.Atoi(os.Getenv("DATABASE_MAX_LIFETIME"))
-	sqlDB.SetMaxIdleConns(maxConn)
+	sqlDB.SetMaxOpenConns(maxConn)
+	sqlDB.SetMaxIdleConns(maxIdleConn)
 	sqlDB.SetConnMaxLifetime(time.Duration(maxLifeTime) * time.Minute)
+
 	helpers.Logger("DATABASE", "Database connection has been established", false)
 	return sqlConnection
 }
 
-func GetSession() gorm.DB {
-	return *sqlConnection
+func GetSession() *gorm.DB {
+	return sqlConnection.Session(&gorm.Session{
+		SkipDefaultTransaction: true,
+	})
 }
